a95daa9eb29e672e0fb5a71d306720e308a1ab04
chan9You <41310752+kyuchanE@users.noreply.github.com>
2021-01-06 11:25:57 +0900

    21.01.06 base setting
    21.01.06 base setting

---
+++ staggered/app/src/main/java/com/chan9u/staggered/activity/adapter/StaggeredAdapter.kt
@@ -0,0 +1,12 @@
+package com.chan9u.staggered.activity.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class StaggeredAdapter {
+
+//    inner class StaggeredViewHolder (
+//            val binding
+//    ): RecyclerView.ViewHolder(){
+//
+//    }
+}
